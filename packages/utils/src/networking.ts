2ef0421005a3abc75a13eaa596c35e1501d528d3
Mike Basdeo <mikebasdeo@gmail.com>
2021-02-25 17:29:29 -0500

    linting ts files

--- packages/utils/src/networking.ts
+++ packages/utils/src/networking.ts
@@ -1,5 +1,5 @@
-import { stringifyError } from "./error";
-import HTTPError from "./http-error";
+import { stringifyError } from './error'
+import HTTPError from './http-error'
 
 /**
  * Make a GET request, and decode the response body as JSON
@@ -10,9 +10,9 @@ import HTTPError from "./http-error";
  */
 async function getJSON(input: RequestInfo, init?: RequestInit) {
   try {
-    const response = await fetch(input, init);
-    const responseJSON = await response.json();
-    return { response, json: responseJSON };
+    const response = await fetch(input, init)
+    const responseJSON = await response.json()
+    return { response, json: responseJSON }
   } catch (err) {
     throw new Error(
       stringifyError(
@@ -21,7 +21,7 @@ async function getJSON(input: RequestInfo, init?: RequestInit) {
         )}`,
         err
       )
-    );
+    )
   }
 }
 
@@ -36,23 +36,23 @@ export async function apiCall(
   path: string,
   init?: RequestInit
 ): Promise<unknown> {
-  let response;
-  let json;
+  let response
+  let json
   try {
-    const jsonRespInfo = await getJSON(`/api/${path}`, init);
-    response = jsonRespInfo.response;
-    json = jsonRespInfo.json;
+    const jsonRespInfo = await getJSON(`/api/${path}`, init)
+    response = jsonRespInfo.response
+    json = jsonRespInfo.json
   } catch (err) {
-    if (err instanceof HTTPError) throw err;
+    if (err instanceof HTTPError) throw err
     throw new Error(
       stringifyError(
         `Networking/apiCall: An error was encountered while making api call to ${path}`,
         err
       )
-    );
+    )
   }
   if (!response.ok)
-    throw new HTTPError(response, "Problem while making API call");
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return json;
+    throw new HTTPError(response, 'Problem while making API call')
+
+  return json as unknown
 }
