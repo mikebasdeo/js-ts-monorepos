baae12e0eacdff9f91f861ec2155aed34574ae8b
Mike Basdeo <mikebasdeo@gmail.com>
2021-03-02 11:39:02 -0500

    feat(utils): updated utils test

--- packages/utils/tests/date.test.ts
+++ packages/utils/tests/date.test.ts
@@ -3,7 +3,7 @@
 import { formatTimestamp } from '../src'
 
 describe('formatTimestamp() tests', function () {
-  let x = 4
+  let x = 5
   test('01-01-2020', () => {
     expect(formatTimestamp(new Date('01-01-2020'))).toBe(
       'Jan 01, 2020 00:01:00 AM'
